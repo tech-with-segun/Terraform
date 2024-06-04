bef13e4500809a759a6818c1512806b41b6b7116
SegunO <161275017+SegunO@users.noreply.github.com>
2024-06-03 20:05:53 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,24 @@
 provider "azurerm" {
   features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
+variable "client_id" {
+  description = "The Client ID for Azure Service Principal"
+}
+
+variable "client_secret" {
+  description = "The Client Secret for Azure Service Principal"
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID for Azure"
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID for Azure"
+}
