65781d27013de5fb7d66d94be3addafbfa85fa1e
SegunO <161275017+SegunO@users.noreply.github.com>
2024-06-03 20:14:16 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "segs-boss-tfstorage-rg"
-    storage_account_name = "seguntfstorage93"
-    container_name       = "tfcontainerbosssegun"
+    resource_group_name  = "segunterrastorage-rg"
+    storage_account_name = "segunterrastorage23"
+    container_name       = "terracontainernowsegun"
     key                  = "terraform.tfstate"
   }
 }
